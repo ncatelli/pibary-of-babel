d4d6399f09fca270aeb6d78f77803ebac3124197
Nate Catelli <ncatelli@packetfire.org>
2022-05-19 16:17:49 -0500

    Add throughtput benchmarks (#1)
    * throughput tests
    
    * args from cli

---
+++ benches/throughput.rs
@@ -0,0 +1,24 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use pibary_of_babel::*;
+
+pub fn generator_throughtput(c: &mut Criterion) {
+    let mut group = c.benchmark_group("1MB of throughput");
+    group.throughput(Throughput::Bytes(1024 * 1024));
+
+    group.bench_function("pi throughtput", |b| {
+        let mut pi = Pi::new();
+        b.iter(|| {
+            let _ = black_box(pi.next());
+        })
+    });
+
+    group.bench_function("byte-generator throughtput", |b| {
+        let mut bg = ByteGenerator::new();
+        b.iter(|| {
+            let _ = black_box(bg.next());
+        })
+    });
+}
+
+criterion_group!(benches, generator_throughtput);
+criterion_main!(benches);
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use pibary_of_babel::*;
 use sliding_windows::{IterExt, Storage};
 
-fn main() {
-    let input = "pi";
+const USAGE: &str = "pibary-of-babel <input>";
+
+fn main() -> Result<(), String> {
+    let args = std::env::args().skip(1).collect::<Vec<_>>();
+    let arg_len = args.len();
+
+    let input = match arg_len {
+        1 => args.get(0).ok_or_else(|| USAGE.to_string()),
+        _ => Err(USAGE.to_string()),
+    }?;
+
     let data = input.as_bytes();
     let mut storage: Storage<u8> = Storage::new(data.len());
 
     let found = ByteGenerator::new()
+        .take(usize::MAX)
         .sliding_windows(&mut storage)
         .position(|window| window == data);
 
     match found {
-        Some(pos_of_data) => println!("{}\n{}..{}", input, pos_of_data, pos_of_data + data.len()),
-        _ => println!("not found: {:?}", &data),
-    };
+        Some(pos_of_data) => {
+            println!("{}..{}", pos_of_data, pos_of_data + data.len());
+            Ok(())
+        }
+        _ => Err(format!("not found: {:?}", &data)),
+    }
 }
