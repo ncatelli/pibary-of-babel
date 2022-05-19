139e3202979e73b58ca67d3a23b518885367b284
Nate Catelli <ncatelli@packetfire.org>
2022-05-19 19:26:21 +0000

    throughput tests

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use pibary_of_babel::*;
 use sliding_windows::{IterExt, Storage};
 
 fn main() {
-    let input = "pi";
+    let input = "ab";
     let data = input.as_bytes();
     let mut storage: Storage<u8> = Storage::new(data.len());
 
     let found = ByteGenerator::new()
+        .take(usize::MAX / 1000000000000000)
         .sliding_windows(&mut storage)
         .position(|window| window == data);
 
