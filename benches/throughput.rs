a7be8a5f7616b619b60dbebeb35f9656f26026e1
Nate Catelli <ncatelli@packetfire.org>
2023-08-13 15:19:59 +0000

    ci: Run benchmarks on main

--- benches/throughput.rs
+++ benches/throughput.rs
@@ -1,37 +1,62 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use pibary_of_babel::*;
 
 pub fn generator_throughtput(c: &mut Criterion) {
-    let mut group = c.benchmark_group("1MB of throughput");
-    group.throughput(Throughput::Bytes(1024 * 1024));
+    let mut group = c.benchmark_group("byte throughput");
 
-    group.bench_function("pi throughtput (spiggot)", |b| {
-        let mut pi = Spigot::new();
-        b.iter(|| {
-            let _ = black_box(pi.next());
-        })
-    });
+    for size in [64, 256] {
+        group.throughput(Throughput::Bytes(size));
+        group.bench_with_input(
+            BenchmarkId::new("pi throughtput (spiggot)", size),
+            &size,
+            |b, bytes_to_read| {
+                let mut pi = Spigot::new();
+                b.iter(|| {
+                    for _ in 0..*bytes_to_read {
+                        let _ = black_box(pi.next());
+                    }
+                })
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("pi throughtput (bellard)", size),
+            &size,
+            |b, bytes_to_read| {
+                let mut pi = BbpBellard::<9>::new(0);
+                b.iter(|| {
+                    for _ in 0..*bytes_to_read {
+                        let _ = black_box(pi.next());
+                    }
+                })
+            },
+        );
 
-    group.bench_function("pi throughtput (bellard)", |b| {
-        let mut pi = BbpBellard::<9>::new(0);
-        b.iter(|| {
-            let _ = black_box(pi.next());
-        })
-    });
+        group.bench_with_input(
+            BenchmarkId::new("byte-generator throughtput (spiggot)", size),
+            &size,
+            |b, bytes_to_read| {
+                let mut bg = ByteGenerator::new(Spigot::new());
+                b.iter(|| {
+                    for _ in 0..*bytes_to_read {
+                        let _ = black_box(bg.next());
+                    }
+                })
+            },
+        );
 
-    group.bench_function("byte-generator throughtput (spiggot)", |b| {
-        let mut bg = ByteGenerator::new(Spigot::new());
-        b.iter(|| {
-            let _ = black_box(bg.next());
-        })
-    });
-
-    group.bench_function("byte-generator throughtput (bellard)", |b| {
-        let mut bg = ByteGenerator::new(BbpBellard::<9>::new(0));
-        b.iter(|| {
-            let _ = black_box(bg.next());
-        })
-    });
+        group.bench_with_input(
+            BenchmarkId::new("byte-generator throughtput (bellard)", size),
+            &size,
+            |b, bytes_to_read| {
+                let mut bg = ByteGenerator::new(BbpBellard::<9>::new(0));
+                b.iter(|| {
+                    for _ in 0..*bytes_to_read {
+                        let _ = black_box(bg.next());
+                    }
+                })
+            },
+        );
+    }
 }
 
 criterion_group!(benches, generator_throughtput);
